7816203b9f7adb85bd5a3f690d59e3392c5ad364
Mirabellensaft <tanks@transfeld.net>
2024-09-15 13:17:28 +0200

    impl wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <WiFiS3.h>
+#include <ArduinoHttpClient.h> 
+#include <Base64.h> 
 #include "secrets.h"
 
 // Variables
@@ -8,7 +10,9 @@ int sensorPin = A0;
 int sensorValue;
 
 // put function declarations here:
-// 
+int send_to_grafana(int value, char* auth);
+char* base64_encoding();
+
 
 void setup() {
   // setup serial communication, 9600 set as a default value
@@ -22,16 +26,33 @@ void setup() {
         delay(500);
         Serial.print(".");
     }
+    
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("wifi connected");
+      Serial.print("IP Adresse: ");
+      Serial.println(WiFi.localIP());
 
-    Serial.println("Connected to wifi.");
-
+      // RSSI (Signalstärke) abrufen
+      long rssi = WiFi.RSSI();
+      Serial.print("Signalstärke (RSSI): ");
+      Serial.println(rssi);
+    } else {
+      Serial.print("error");
+    }
 }
 
 void loop() {
+  Serial.print("enter void");
   int sensorValue = analogRead(sensorPin); 
   Serial.print("Analog Sensor Value: ");
   Serial.println(sensorValue); 
+  Serial.print("IP Adresse: ");
+  Serial.println(WiFi.localIP());
+  long rssi = WiFi.RSSI();
+  Serial.print("Signalstärke (RSSI): ");
+  Serial.println(rssi);
   delay(1000);  
 }
 
 // put function definitions here:
+
